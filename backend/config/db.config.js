0827dde7ff85259efbb6f0230bad4a52a50c77ff
mushthaq <techcorpian@gmail.com>
2024-08-08 16:29:22 +0530

    updated to mysql2 and env bug fixed

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,12 +1,12 @@
-import mysql from 'mysql'
+import mysql from 'mysql2'
 import * as dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql.createConnection({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
   });
 
 pool.connect(error => {
